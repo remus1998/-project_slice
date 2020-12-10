f6ee83e4e861294d5ae4c18ebdbde8dfd47c3404
Remus <mprekuta@gmail.com>
2020-12-10 09:30:14 +0100

    project steps commited

---
+++ js/script.js
@@ -0,0 +1,23 @@
+//to-do applikáció készítés alapértelmezésben 10 üres mező áll rendelkezésre de hozzá tudunk adni ujjakat és minden mezőben van egy checkbox ahol bepipálhatjuk hogy kész van e a feladat és van egy text input mezőnka ahova beirhatjuk a feladatleírást és egy törlés gombunk és hozzá is tudunk adni
+function pageLoad(){ 
+// létre kell hoznom egy változót amibe belerakom a rootot
+
+
+//el kell tárolnunk egy változóban a sorok számát majd el kéne menteni a változóba egy sort is amibe kelleni fog egy div/checkbox/button ebből fog állni egy sor amit duplikálni fogunk 
+
+
+
+//amit készítettünk azt most duplikáljuk 10x a forciklusba insert.adjastmentHTMLel hozzáadjuk a sorokat
+
+
+
+//(afterend)rooton kívül kell egy gomb a végére amivel még hozzá tudunk adni 1et a végére (div mezőt) 
+
+
+//ehhez a gombhoz egy click eseményt hozzáadni 
+
+
+//queryselector(all) classnév alapján kijelölöm az összes törlés gombot és elmentem egy változóba.
+
+}
+window.addEventListener("load",pageLoad);
